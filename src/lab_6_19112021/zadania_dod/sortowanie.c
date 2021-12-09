fb7b955da3d89a18be2b30c1f74594942dbd0af6
curaposterior <empo123@interia.pl>
2021-12-09 10:59:55 +0100

    adding quicksort

--- src/lab_6_19112021/zadania_dod/sortowanie.c
+++ src/lab_6_19112021/zadania_dod/sortowanie.c
@@ -28,6 +28,33 @@ void insertion_sort(int tab[], int rozmiar) {
     } 
 }
 
+/*Implementacja algorytmu sortowania szybkiego
+z ksiazki "Jezyk ANSI C. Programowanie" 
+Brian W. Kernighan
+Dennis M. Ritchie*/
+void qsort(int v[], int left, int right) {
+    int i, last;
+    void swap(int v[], int i, int j);
+
+    if (left >= right)
+        return;
+    swap(v, left, (left + right) / 2);
+    last = left;
+    for (i = left + 1; i <= right; i++)
+        if (v[i] < v[left])
+            swap(v, ++last, i);
+    swap(v, left, last);
+    qsort(v, left, last-1);
+    qsort(v, last + 1, right);
+}
+
+void swap(int v[], int i, int j) {
+    int temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
 int main() {
     int rozmiar = 10;
     int tab1[10] = {10,9,8,7,6,5,4,3,1,2};
